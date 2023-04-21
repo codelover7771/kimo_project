e31641d259ca535ca5e5d22719d67e4f0a1bc4df
codelover7771 <umeshramyadav0022@gmail.com>
2023-04-21 08:01:15 +0530

    minor change in highlights section

--- kimo-app/src/components/Highlights.jsx
+++ kimo-app/src/components/Highlights.jsx
@@ -16,7 +16,7 @@ export default function Highlights (){
   
   return (
     <div className='w-full pb-12 justify-center items-center'>
-        <div className='w-5/6 m-auto mt-[24px] md:w-[95%]'>
+        <div className='w-5/6 m-auto mt-[24px] md:w-3[95%]'>
             <div className='h-[80px] flex items-center'>
                 <p className='heading'>Highlights</p>
             </div>
